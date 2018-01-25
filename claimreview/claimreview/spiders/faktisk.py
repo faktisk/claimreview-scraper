57be9f6747537109ea51ff8b9587879e52a9bece
Jari Bakken <jari.bakken@gmail.com>
2018-01-25 14:09:04 +0100

    Add faktisk scraper

--- claimreview/claimreview/spiders/faktisk.py
+++ claimreview/claimreview/spiders/faktisk.py
@@ -1,15 +1,35 @@
-# import scrapy
-# from claimreview.spiders.claimreview import ClaimReviewSpider
-# from scrapy.spiders import Rule
-# from scrapy.linkextractors import LinkExtractor
+import scrapy
+from scrapy.spiders import XMLFeedSpider
+from claimreview.spiders.claimreview import ClaimReviewSpider
+from claimreview.parser import ClaimReviewParser
 
-# class FaktiskSpider(XMLFeedSpider):
-#     name = 'faktisk'
-#     allowed_domains = ['www.faktisk.no']
-#     start_urls = [ 'https://www.faktisk.no/atom.xml' ]
+class FaktiskSpider(XMLFeedSpider):
+    name = 'faktisk'
 
-#     rules = (
-#         Rule(LinkExtractor(allow='/news/fact-checker/'), callback='parse_item'),
-#     )
+    allowed_domains = [
+      'www.faktisk.no'
+    ]
+
+    start_urls = [
+      'https://www.faktisk.no/atom.xml'
+    ]
+
+    iterator = 'xml'
+    namespaces = [('atom', 'http://www.w3.org/2005/Atom')]
+    itertag = 'atom:entry'
+
+    parser = ClaimReviewParser()
+
+    def parse_node(self, response, node):
+      html_page = node.xpath('./atom:link[@rel="alternate" and @type="text/html"]/@href').extract_first()
+      self.logger.info(html_page)
+
+      return response.follow(html_page, callback=self.parse_html)
+
+    def parse_html(self, response):
+      items = self.parser.parse(response)
+      for item in items:
+        yield item
+
 
 
